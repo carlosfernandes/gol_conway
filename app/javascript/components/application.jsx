731cb8310f3c6ad9f78c616833d569f43040e2c7
Carlos Fernandes <carneirofernandes.carlos@gmail.com>
2024-02-17 14:26:39 -0300

    Add react

---
+++ app/javascript/components/application.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Application = () => {
+  return (
+    <div>
+      <h1>Hello, Rails 7!</h1>
+    </div>
+  )
+}
+
+ReactDOM.render(<Application />, document.getElementById('root'))
