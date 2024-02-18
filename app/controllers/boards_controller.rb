479c4f93c5c956568e7e35c10b9c7f95812eac27
Carlos Fernandes <carneirofernandes.carlos@gmail.com>
2024-02-18 15:40:20 -0300

    Refactor Boards Controller

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,6 +2,22 @@ class BoardsController < ApplicationController
   def index
     @boards = Board.all
 
-    render json: { boards: @boards }
+    render json: { boards: @boards.select(:id, :name) }
   end
+
+  def create
+    @board = Board.new(name: board_params[:name], size: board_params[:size])
+    @board.generations.build(state: board_params[:initial_state])
+
+    if @board.save
+      render json: { id: @board.id, name: @board.name }, status: :created
+    else
+      render json: @board.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def board_params
+      params.require(:board).permit(:name, :size, :initial_state)
+    end
 end
